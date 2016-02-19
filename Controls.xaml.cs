512dbb3df4d8e75491503f1832c14f83f423f60c
lucasx <lucasx@stanford.edu>
2016-02-18 21:33:41 -0800

    Initial commit

---
+++ Controls.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_WallpaperCrop_v2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Controls : Window
+    {
+        Preview preview;
+
+        public Controls()
+        {
+            InitializeComponent();
+
+            // Launch second window
+            preview = new Preview(this);
+            preview.Show();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            maximizeOnRightmostScreen();
+        }
+
+        private void maximizeOnRightmostScreen()
+        {
+            Screen[] sc = Screen.AllScreens;
+            int nScreens = sc.Length;
+
+            // Show this window on the rightmost screen
+            if (nScreens > 1)
+            {
+                Screen rightmostScreen = sc[nScreens - 1];
+                this.Left = rightmostScreen.Bounds.Left;
+                this.Top = rightmostScreen.Bounds.Top;
+            }
+
+            // Maximize window
+            this.WindowState = WindowState.Maximized; // If I remember correctly maximizeOnRightmostScreen() must be called after window load for this to work.
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            preview.Close();
+        }
+
+        ///////////////////////////////// Control functionality stuff //////////////////////////////////////////
+
+        private void chooseImage(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog chooseImage = new OpenFileDialog();
+            chooseImage.Title = "Choose Image";
+            chooseImage.Filter = "JPG|*.jpg|PNG|*.png";
+            if (chooseImage.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                BitmapImage bitmapimage = new BitmapImage(new Uri(chooseImage.SafeFileName, UriKind.Relative));
+                image.Source = bitmapimage;
+                preview.setImage(bitmapimage);
+            }
+        }
+
+        private void centerImage(object sender, RoutedEventArgs e)
+        {
+            preview.centerImage();
+        }
+    }
+}
---
+++ Preview.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_WallpaperCrop_v2
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Preview : Window
+    {
+        Controls controls;
+        public Preview(Controls c)
+        {
+            InitializeComponent();
+
+            controls = c;
+            configureWindow();
+        }
+
+        ////////////////////////// Window Stuff ///////////////////////////////
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            configureDragging();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                controls.Close();
+            }
+        }
+
+        /* Sets up window frame with no borders and sets it to span
+         * the left two screens: all except the rightmost one, where the controls are.
+         * Usage: Must be called before window is loaded in order to turn borders off. */
+        // TODO: Make it work for different monitor configurations, perhaps with dialog box asking about monitor order
+        private void configureWindow()
+        {
+            int nMonitors = System.Windows.Forms.SystemInformation.MonitorCount;
+            Debug.Assert(nMonitors > 1);
+
+            this.WindowStyle = WindowStyle.None; // Gets rid of top bar
+            this.AllowsTransparency = true; // Gets rid of window-resize borders
+
+            // span left two monitors
+            this.Left = -1920;
+            this.Width = System.Windows.SystemParameters.VirtualScreenWidth * 2 / 3;
+            this.Top = 0;
+            this.Height = System.Windows.SystemParameters.VirtualScreenHeight;
+        }
+
+
+        ///////////////////////////////// Functionality stuff ///////////////////////////////////
+
+        /// Functionality to move image by dragging mouse ///
+        Point basePoint;
+        bool dragging = false;
+
+        /* Register mouse event handlers */
+        public void configureDragging()
+        {
+            image.MouseLeftButtonDown += (ss, ee) =>
+            {
+                dragging = true;
+                basePoint = ee.GetPosition(this);
+                image.CaptureMouse();
+            };
+
+            image.MouseMove += (ss, ee) =>
+            {
+                if (dragging == true)
+                {
+                    // Get distance moved
+                    Point newPoint = ee.GetPosition(this);
+                    Point diff = new Point(newPoint.X - basePoint.X, newPoint.Y - basePoint.Y);
+
+                    // Update image
+                    Canvas.SetLeft(image, Canvas.GetLeft(image) + diff.X);
+                    Canvas.SetTop(image, Canvas.GetTop(image) + diff.Y);
+
+                    basePoint = newPoint;
+                }
+            };
+
+            image.MouseLeftButtonUp += (ss, ee) => {
+                image.ReleaseMouseCapture();
+                dragging = false;
+            };
+        }
+
+        internal void setImage(BitmapImage bitmapImage)
+        {
+            image.Source = bitmapImage;
+
+            // size image to actual size
+            image.Width = bitmapImage.PixelWidth;
+            image.Height = bitmapImage.PixelHeight;
+
+            centerImage();
+        }
+
+        internal void centerImage()
+        {
+            Canvas.SetLeft(image, -image.Width / 2);
+            Canvas.SetTop(image, -image.Height / 2);
+        }
+    }
+}
